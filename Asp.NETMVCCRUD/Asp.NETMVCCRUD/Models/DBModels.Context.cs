5b56cc9b94229f75a042cb8a618e483bc868406c
Natnael Mulugeta <natnaelmu.07@gmail.com>
2020-01-19 18:05:19 +1100

    MVC5 .NET CRUD

---
+++ Asp.NETMVCCRUD/Asp.NETMVCCRUD/Models/DBModels.Context.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Asp.NETMVCCRUD.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Infrastructure;
+    
+    public partial class DBModel : DbContext
+    {
+        public DBModel()
+            : base("name=DBModel")
+        {
+        }
+    
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            throw new UnintentionalCodeFirstException();
+        }
+    
+        public virtual DbSet<Employee> Employees { get; set; }
+    }
+}
